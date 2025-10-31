b3dd4a76de95496d3aa38d338456bb8b7ce9198e
Jacobdrosol <120150967+Jacobdrosol@users.noreply.github.com>
2025-10-31 13:55:18 -0500

    Update version.py

--- assistant_app/version.py
+++ assistant_app/version.py
@@ -1,10 +1,6 @@
 __all__ = ["__version__"]
 
 # Update this value when creating a new release tag.
-<<<<<<< HEAD
 __version__ = "0.1.11"
 
 # Testing Updater with a new version
-=======
-__version__ = "0.1.10"
->>>>>>> f166e89aeb9e8bab11f0951265caa432177969f3
